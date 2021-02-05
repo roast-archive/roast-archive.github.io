f3ed4ab5bad3f59c2531b183c8e647ba6e276fb9
Jesse Smith <jesse.etan.smith@gmail.com>
2021-02-05 11:16:05 -0500

    passwords

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 /*
  	Hello, my love!
 */
-function pass_to_archive() {
+function pass_to_rst_archive() {
 
-	var pass_try = prompt("Please enter the archives\' password.")
-  if(pass_try == "poet ruffian") {
+	var pass_try = prompt("Please enter the roast archives\' password.")
+  if(pass_try.lower() == "poet ruffian") {
+      return true;
+  } else {
+      alert('Sorry, that\'s not correct.');
+      return false;
+  }
+
+}
+
+function pass_to_sys_archive() {
+
+	var pass_try = prompt("Please enter the system archives\' password.")
+  if(pass_try.lower() == "twenty two am") {
       return true;
   } else {
       alert('Sorry, that\'s not correct.');
