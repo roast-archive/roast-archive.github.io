8a503a6ef526df73b822ba11d689864456980584
Jesse Smith <jesse.etan.smith@gmail.com>
2021-02-05 17:17:54 -0500

    tolowercase fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function pass_to_rst_archive() {
 
 	var pass_try = prompt("Please enter the roast archives\' password.")
-  if(pass_try.lower() == "poet ruffian") {
+  if(pass_try.toLowerCase() == "poet ruffian") {
       return true;
   } else {
       alert('Sorry, that\'s not correct.');
@@ -16,7 +16,7 @@ function pass_to_rst_archive() {
 function pass_to_sys_archive() {
 
 	var pass_try = prompt("Please enter the system archives\' password.")
-  if(pass_try.lower() == "twenty two am") {
+  if(pass_try.toLowerCase() == "twenty two am") {
       return true;
   } else {
       alert('Sorry, that\'s not correct.');
